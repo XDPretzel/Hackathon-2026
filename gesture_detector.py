786f6d360a36dca33445ba45cb0893677adfd633
XDPretzel <123917419+XDPretzel@users.noreply.github.com>
2026-05-02 15:26:29 -0700

    Created skeleton for hand gestures

---
+++ gesture_detector.py
@@ -0,0 +1,34 @@
+#=============== Import libraries ===============
+import cv2
+import mediapipe as mp
+
+
+#=============== Play Gesture detection ===============
+def play_gesture():
+    pass
+
+#=============== Pause Gesture detection ===============
+def pause_gesture():
+    pass
+
+
+#=============== Skip Gesture detection ===============
+def skip_gesture():
+    pass
+
+#=============== Previous Gesture detection ===============
+def previous_gesture():
+    pass
+
+
+#=============== Volume Up Gesture detection ===============
+def volume_up_gesture():
+    pass
+
+#=============== Volume Down Gesture detection ===============
+def volume_down_gesture():
+    pass
+
+#=============== Save/Like Gesture detection ===============
+def save_like_gesture():
+    pass
